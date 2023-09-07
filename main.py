56dc6e0f252e8ca674dbb32b50c4584099aad947
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-09-07 15:45:50 +0200

    save offer to csv

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
+import csv
+
+all_offers_from_given_dates = []
 
 
 def init_driver(link_to_scrape):
@@ -41,17 +44,18 @@ def init_driver(link_to_scrape):
 
     date_range_to_find_offers_start = 15
 
-    while date_range_to_find_offers_start < 23:
+    while date_range_to_find_offers_start <= 17:
 
         dropdown_dates = driver.find_element(By.CSS_SELECTOR,
                                              'button[data-testid="dropdown-field--travel-date"]')
         dropdown_dates.click()
         time.sleep(1)
         specific_day_offers = driver.find_element(By.CSS_SELECTOR,
-                                             'div[data-tab-id="tab-gs-travelDate-single"]')
+                                                  'div[data-tab-id="tab-gs-travelDate-single"]')
         specific_day_offers.click()
         time.sleep(1)
-        offers_arrivals_from_date = driver.find_element(By.CSS_SELECTOR, f'time[datetime="2023-09-{date_range_to_find_offers_start}T00:00:00.000"]')
+        offers_arrivals_from_date = driver.find_element(By.CSS_SELECTOR,
+                                                        f'time[datetime="2023-09-{date_range_to_find_offers_start}T00:00:00.000"]')
         offers_arrivals_from_date.click()
         time.sleep(1)
         dropdown_date_period_arrivals_submit = driver.find_element(By.CSS_SELECTOR,
@@ -72,11 +76,13 @@ def init_driver(link_to_scrape):
             except NoSuchElementException:
                 hotel = None
             try:
-                country = offer.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('destination').split(", ")[0]
+                country = \
+                offer.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('destination').split(", ")[0]
             except NoSuchElementException:
                 country = None
             try:
-                region = offer.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('destination').split(", ")[1:]
+                region = offer.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('destination').split(
+                    ", ")[1:]
             except NoSuchElementException:
                 region = None
             try:
@@ -84,19 +90,24 @@ def init_driver(link_to_scrape):
             except NoSuchElementException:
                 offer_link = None
             try:
-                trip_advisor_rating = offer.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text.split("\n")[0]
+                trip_advisor_rating = \
+                offer.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text.split("\n")[0]
             except NoSuchElementException:
                 trip_advisor_rating = None
             try:
-                trip_advisor_opinions = offer.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text.split("\n")[1]
+                trip_advisor_opinions = \
+                offer.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text.split("\n")[1]
             except NoSuchElementException:
                 trip_advisor_opinions = None
             try:
-                departure_airport = offer.find_element(By.CSS_SELECTOR, 'div[data-testid="dropdown--same-day-offers"]').text.split(" ")[0]
+                departure_airport = \
+                offer.find_element(By.CSS_SELECTOR, 'div[data-testid="dropdown--same-day-offers"]').text.split(" ")[0]
             except NoSuchElementException:
                 departure_airport = None
             try:
-                departure_time = offer.find_element(By.CSS_SELECTOR, 'div[data-testid="dropdown--same-day-offers"]').text.split(" ")[1].replace("(", "").replace(")", "")
+                departure_time = \
+                offer.find_element(By.CSS_SELECTOR, 'div[data-testid="dropdown--same-day-offers"]').text.split(" ")[
+                    1].replace("(", "").replace(")", "")
             except NoSuchElementException:
                 departure_time = None
             try:
@@ -112,24 +123,48 @@ def init_driver(link_to_scrape):
             except NoSuchElementException:
                 board_type = None
             try:
-                offer_dates = offer.find_element(By.CSS_SELECTOR, 'span[data-testid="offer-tile-departure-date"]').text
+                offer_date = offer.find_element(By.CSS_SELECTOR, 'span[data-testid="offer-tile-departure-date"]').text
             except NoSuchElementException:
-                offer_dates = None
-            print(f"Hotel: {hotel}")
-            print(f"country: {country}")
-            print(f"region : {region}")
-            print(f"trip_advisor_rating: {trip_advisor_rating}")
-            print(f"trip_advisor_opinions: {trip_advisor_opinions}")
-            print(f"departure_airport: {departure_airport}")
-            print(f"departure_time: {departure_time}")
-            print(f"price: {price}")
-            print(f"currency: {currency}")
-            print(f"board_type: {board_type}")
-            print(f"offer_dates: {offer_dates}")
-            print(f"offer_link: {offer_link}")
+                offer_date = None
+
+            single_offer = {
+                "hotel": hotel,
+                "country": country,
+                "region": region,
+                "trip_advisor_rating": trip_advisor_rating,
+                "trip_advisor_opinions": trip_advisor_opinions,
+                "departure_airport": departure_airport,
+                "departure_time": departure_time,
+                "price": price,
+                "currency": currency,
+                "board_type": board_type,
+                "offer_date": offer_date,
+                "offer_link": offer_link
+            }
+            all_offers_from_given_dates.append(single_offer)
+
+            # print(f"Hotel: {hotel}")
+            # print(f"country: {country}")
+            # print(f"region : {region}")
+            # print(f"trip_advisor_rating: {trip_advisor_rating}")
+            # print(f"trip_advisor_opinions: {trip_advisor_opinions}")
+            # print(f"departure_airport: {departure_airport}")
+            # print(f"departure_time: {departure_time}")
+            # print(f"price: {price}")
+            # print(f"currency: {currency}")
+            # print(f"board_type: {board_type}")
+            # print(f"offer_date: {offer_date}")
+            # print(f"offer_link: {offer_link}")
+
         date_range_to_find_offers_start += 1
         time.sleep(5)
+    tui_offers = f"tui_offers_from_day_to.csv"
+    fields = ["hotel", "country", "region", "offer_link", "trip_advisor_rating", "trip_advisor_opinions",
+              "departure_airport", "departure_time", "price", "currency", "board_type", "offer_date"]
+    with open(tui_offers, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(all_offers_from_given_dates)
     driver.quit()
-
 
 init_driver("https://www.tui.pl/")
