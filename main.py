717a0bd1202eeeb15a7d4633d2b289911f42ed30
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-09-07 10:32:10 +0200

    add getting of more details from offer: country, region, trip advisor details, departure time/airport, currency, link to offer

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def init_driver(link_to_scrape):
     window_after = driver.window_handles[1]
     driver.switch_to.window(window_after)
     time.sleep(3)
-    #choosing list of airport from where last minute offers should be shown
+    # choosing list of airport from where last minute offers should be shown
     dropdown_airports_of_departure = driver.find_element(By.CSS_SELECTOR,
                                                          'button[data-testid="dropdown-field--airport"]')
     dropdown_airports_of_departure.click()
@@ -34,12 +34,12 @@ def init_driver(link_to_scrape):
     airports = driver.find_elements(By.CLASS_NAME, "bp3-control-indicator")[10]
     airports.click()
     dropdown_airports_of_departure_submit = driver.find_element(By.CSS_SELECTOR,
-                                                         'button[data-testid="dropdown-window-button-submit"]')
+                                                                'button[data-testid="dropdown-window-button-submit"]')
     dropdown_airports_of_departure_submit.click()
     time.sleep(3)
-    #choosing date period of arrivals
+    # choosing date period of arrivals
     dropdown_dates = driver.find_element(By.CSS_SELECTOR,
-                                                         'button[data-testid="dropdown-field--travel-date"]')
+                                         'button[data-testid="dropdown-field--travel-date"]')
     dropdown_dates.click()
     time.sleep(1)
     offers_arrivals_from_date = driver.find_element(By.CSS_SELECTOR, 'time[datetime="2023-09-15T00:00:00.000"]')
@@ -47,28 +47,48 @@ def init_driver(link_to_scrape):
     offers_arrivals_to_date = driver.find_element(By.CSS_SELECTOR, 'time[datetime="2023-09-23T00:00:00.000"]')
     offers_arrivals_to_date.click()
     dropdown_date_period_arrivals_submit = driver.find_element(By.CSS_SELECTOR,
-                                                                'button[data-testid="dropdown-window-button-submit"]')
+                                                               'button[data-testid="dropdown-window-button-submit"]')
     dropdown_date_period_arrivals_submit.click()
 
     time.sleep(2)
 
     global_search_button_submit = driver.find_element(By.CSS_SELECTOR,
-                                                               'button[data-testid="global-search-button-submit"]')
+                                                      'button[data-testid="global-search-button-submit"]')
     global_search_button_submit.click()
     time.sleep(15)
-    offer = driver.find_element(By.CSS_SELECTOR,'div[data-testid="offer-tile"]').text
     hotel = driver.find_element(By.CLASS_NAME, 'offer-tile-body__header').text
-    country = driver.find_element(By.CSS_SELECTOR, 'nav[data-testid="offer-tile-breadcrumbs"]').text
-    trip_advisor_opinion = driver.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text
+    country = driver.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('destination').split(", ")[0]
+    region = driver.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('destination').split(", ")[1:]
+    offer_link = driver.find_element(By.XPATH, f'//a[@hotelname="{hotel}"]').get_attribute('href')
+    trip_advisor_rating = \
+    driver.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text.split("\n")[0]
+    trip_advisor_opinions = \
+    driver.find_element(By.CSS_SELECTOR, 'div[data-testid="tripadvisor-opinions-badge"]').text.split("\n")[1]
+    departure_airport = \
+    driver.find_element(By.CSS_SELECTOR, 'div[data-testid="dropdown--same-day-offers"]').text.split(" ")[0]
+    departure_time = \
+    driver.find_element(By.CSS_SELECTOR, 'div[data-testid="dropdown--same-day-offers"]').text.split(" ")[1].replace("(",
+                                                                                                                    "").replace(
+        ")", "")
     price = driver.find_element(By.CSS_SELECTOR, 'span[data-testid="price-amount"]').text.replace(" ", "")
+    currency = driver.find_element(By.CLASS_NAME, 'price-value__currency').text
     board_type = driver.find_element(By.CSS_SELECTOR, 'span[data-testid="offer-tile-boardType"]').text
     offer_dates = driver.find_element(By.CSS_SELECTOR, 'span[data-testid="offer-tile-departure-date"]').text
 
-    print(offer)
-
+    print(f"Hotel: {hotel}")
+    print(f"country: {country}")
+    print(f"region : {region}")
+    print(f"trip_advisor_rating: {trip_advisor_rating}")
+    print(f"trip_advisor_opinions: {trip_advisor_opinions}")
+    print(f"departure_airport: {departure_airport}")
+    print(f"departure_time: {departure_time}")
+    print(f"price: {price}")
+    print(f"currency: {currency}")
+    print(f"board_type: {board_type}")
+    print(f"offer_dates: {offer_dates}")
+    print(f"offer_link: {offer_link}")
     time.sleep(300)
     driver.quit()
 
 
 init_driver("https://www.tui.pl/")
-
