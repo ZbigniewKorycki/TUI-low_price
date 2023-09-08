fb93eb5b3e46466916ecc254d1bae40dea8ad1f3
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-09-08 18:07:43 +0200

    rename variables, simplify function extracting offers with rating 4

--- data.py
+++ data.py
@@ -2,17 +2,16 @@ import pandas as pd
 
 offers = pd.read_csv("TUI_last_minute_offers.csv", encoding='ISO-8859-2', encoding_errors='replace')
 
+not_acceptable_ratings = ['2/5', '2.5/5', '3/5', '3.5/5']
+not_acceptable_countries = ['Bułgaria', 'Albania', 'Węgry', 'Czechy', 'Austria']
+not_acceptable_hotels = ['Savvas', 'Marianna Apart Hotel']
+
 offers_price_below_1500 = offers.loc[offers['price'] <= 1500]
 offers_price_below_1250 = offers.loc[offers['price'] <= 1250]
 offers_price_below_1000 = offers.loc[offers['price'] <= 1000]
 
-offers_with_rating_above_4_or_none = offers.loc[
-    (pd.isna(offers['trip_advisor_rating'])) | (offers['trip_advisor_rating'] == '4.5/5') | (
-                offers['trip_advisor_rating'] == '4/5')]
-
-countries_offer_to_delete = ['Bułgaria', 'Albania', 'Węgry', 'Czechy', 'Austria']
-
-offers_with_wished_countries = offers[~offers['country'].isin(countries_offer_to_delete)]
+offers_with_rating_above_4_or_none = offers[~offers['trip_advisor_rating'].isin(not_acceptable_ratings)]
+offers_with_wished_countries = offers[~offers['country'].isin(not_acceptable_countries)]
 
 offers_above_4_wished_countries_below_1500 = pd.merge(
     pd.merge(offers_with_rating_above_4_or_none, offers_with_wished_countries, how='inner'), offers_price_below_1500,
@@ -24,14 +23,13 @@ offers_above_4_wished_countries_below_1000 = pd.merge(
     pd.merge(offers_with_rating_above_4_or_none, offers_with_wished_countries, how='inner'), offers_price_below_1000,
     how="inner")
 
-bad_hotels = ['Savvas', 'Marianna Apart Hotel']
 
 final_offers_1500 = offers_above_4_wished_countries_below_1500[
-    ~offers_above_4_wished_countries_below_1500['hotel'].isin(bad_hotels)]
+    ~offers_above_4_wished_countries_below_1500['hotel'].isin(not_acceptable_hotels)]
 final_offers_1250 = offers_above_4_wished_countries_below_1250[
-    ~offers_above_4_wished_countries_below_1250['hotel'].isin(bad_hotels)]
+    ~offers_above_4_wished_countries_below_1250['hotel'].isin(not_acceptable_hotels)]
 final_offers_1000 = offers_above_4_wished_countries_below_1000[
-    ~offers_above_4_wished_countries_below_1000['hotel'].isin(bad_hotels)]
+    ~offers_above_4_wished_countries_below_1000['hotel'].isin(not_acceptable_hotels)]
 
 if not final_offers_1500.empty:
     print(final_offers_1500)
@@ -40,8 +38,3 @@ if not final_offers_1250.empty:
 if not final_offers_1000.empty:
     print(final_offers_1000)
 
-# print(offers)
-#
-# print(offers_price_below_1500)
-# print(offers_price_below_1200.empty)
-# print(offers_price_below_1000.empty)
